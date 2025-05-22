8a2583bf59eec8eb1e540fed654f56784d808aec
masenyetse33 <judemasenyetse@hotmail.com>
2025-05-22 11:00:08 +0200

    Added get_entry(), update_entry() and delete_entry() functions. Created a match-case based value and dumbed down add_entry for now

--- password_manager/manager.py
+++ password_manager/manager.py
@@ -1,16 +1,16 @@
-import re
+entries = {}
 
 class Manager:
-    def __init__(self, password = "password9", program_running = False):
-        self.password = password
+    def __init__(self, master_password = "password9", program_running = False):
+        self.master_password = master_password
         self.program_running = program_running
     
-    def master_password(self):
+    def check_master_password(self):
         wrong_attempts = 0
 
         while wrong_attempts < 3:
-            password_attempt = input("Enter the password: ")
-            if password_attempt == self.password:
+            password_attempt = input("Enter the master password: ")
+            if password_attempt == self.master_password:
                 self.program_running = True
                 break
             else:
@@ -21,34 +21,78 @@ class Manager:
                     print("You've entered the wrong password three times and have been locked out.")
                     self.program_running = False
     
-    def get_websites(self):
-        try:
-            with open("secrets.txt", "r", encoding="utf-8") as f:
-                text = f.read()
-            websites = re.findall(r'(?:^|\s)(\S*?\.com\S*?)(?=\s|$)', text)
-            return websites
-        except FileNotFoundError:
-            return []
-    
     def add_entry(self, site, username, password):
-
-        existing_websites = self.get_websites()
-
-        if site in existing_websites:
-            print("That website has already been registered.")
-            return
+        entries[site] = username, password
+        entry = f"""\n{site} = username: "{username}", password: "{password}" """
+        with open("secrets.txt", "a") as secrets:
+            secrets.write(entry)
+        print("Entry added succesfuly.")
+    
+    def get_entry(self, site):
+        for key in entries:
+            if key == site:
+                value = entries[site]
+                print(value)
+            else:
+                print("Site not found.")
+    
+    def update_entry(self, site, new_username, new_password):
+        for key in entries:
+            if key == site:
+                entries[key] = new_username, new_password
+                print("Entry updated succesfully.")
+            else:
+                print("That site has not been registered before.")
+    
+    def delete_entry(self, site):
+        for key in entries:
+            if key == site:
+                del entries[key]
+                print("Entry deleted succesfully.")
+                break
+            else:
+                print("That site does not exist.")
+    
+    def list_entries(self):
+        if len(entries) == 0:
+            print("There are no entries.")
         else:
-            entry = f"""\n{site} = username: "{username}", password: "{password}" """
-            with open("secrets.txt", "a") as secrets:
-                secrets.write(entry)
-        
+            print(entries)
 
 manager = Manager()
 
-manager.master_password()
+manager.check_master_password()
 
-site = input("Enter the site: ")
-username = input("Enter the username: ")
-password = input("Enter the password: ")
+while manager.program_running == True:
+    user_choice = input("""What would you like to do?
+                        Choose 1 to add an entry.
+                        Choose 2 to get an entry.
+                        Choose 3 to update an entry.
+                        Choose 4 to delete an entry.
+                        Choose 5 to list all entries.
+                        Choose 6 to exit.""")
 
-manager.add_entry(site, username, password)
+    match user_choice:
+        case "1":
+            site = input("Enter the site: ")
+            username = input("Enter the username: ")
+            password = input("Enter the password: ")
+            manager.add_entry(site, username, password)
+        case "2":
+            site = input("Enter the site of the credentials you'd like to retrieve: ")
+            manager.get_entry(site)
+        case "3":
+            site_to_update = input("Enter the site you'd like to update the credentials of: ")
+            new_username = input("Enter the new username: ")
+            new_password = input("Enter the new password: ")
+            manager.update_entry(site_to_update, new_username, new_password)
+        case "4":
+            site_to_delete = input("Enter the site of the entry you'd like to delete: ")
+            manager.delete_entry(site_to_delete)
+        case "5":
+            manager.list_entries()
+        case "6":
+            print("Goodbye")
+            manager.program_running = False
+        case _:
+            print("Choose a valid option")
